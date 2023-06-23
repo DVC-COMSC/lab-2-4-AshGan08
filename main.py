0ed76d103f27ffc7e3c742f7962aff1f0ffc6db8
AshGan08 <agandluru897@insite.4cd.edu>
2023-06-23 12:24:10 -0700

    Slicing finished

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    original_str = "python programming  "
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:19]
+    merged_str = sub2 + sub1
+    return sub1, sub2, merged_str
 
 
 if __name__ == '__main__':
-    main()
+    sub1, sub2, merged_str = main()
+    print(sub2)
+    print(sub1)
+    print(merged_str)
