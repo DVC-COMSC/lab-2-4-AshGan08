75f9de86b96d7ca83ed90e5b95cf28f3c2b7f069
AshGan08 <agandluru897@insite.4cd.edu>
2023-06-24 11:09:27 -0700

    done

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def main():
-    original_str = "python programming  "
+    original_str = "Python Programming"
     sub1 = original_str[0:6]
-    sub2 = original_str[7:19]
-    merged_str = sub2 + sub1
-    return sub1, sub2, merged_str
+    sub2 = original_str[7:18]
+    merge_str = sub2 + ' '+ sub1
+    return sub1, sub2, merge_str
 
 
 if __name__ == '__main__':
-    sub1, sub2, merged_str = main()
+    sub1, sub2, merge_str = main()
     print(sub2)
     print(sub1)
-    print(merged_str)
+    print(merge_str)
