9d52dd7e845f343e5784a447cf9cb13f08cd2efc
AshGan08 <agandluru897@insite.4cd.edu>
2023-06-24 11:53:20 -0700

    done...

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     original_str = "Python Programming"
-    sub1 = original_str[0:6]
-    sub2 = original_str[7:18]
+    sub1 = original_str[:6]
+    sub2 = original_str[7:]
     merged_str = sub2 + sub1
     return sub1, sub2, merged_str
 
