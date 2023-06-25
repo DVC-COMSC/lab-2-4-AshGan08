b177c290c68e52619cebe0eabeb4c37c18be470c
Kyuwoong Lee <kwooler@gmail.com>
2023-06-24 22:14:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ def main():
     sub1 = original_str[:6]
     sub2 = original_str[7:]
     merged_str = sub2 + sub1
+    print(sub2)
+    print(sub1)
+    print(merged_str)
     return sub1, sub2, merged_str
 
 
 if __name__ == '__main__':
     sub1, sub2, merged_str = main()
-    print(sub2)
-    print(sub1)
-    print(merged_str)
+   
